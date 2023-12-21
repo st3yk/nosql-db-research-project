a9376ea7a29bd991f43daa87b5ffb9fa1d2c2705
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-12-21 22:01:20 +0100

    Change data model

--- benchmark/Model.cs
+++ benchmark/Model.cs
@@ -3,12 +3,43 @@ using MongoDB.Bson.Serialization;
 
 namespace Models{
     public class Data{
-        [BsonElement("field1")]
-        public double field1 {get; set;}
-        [BsonElement("field2")]
-        public string field2 {get; set;}
-        [BsonElement("field3")]
-        public string field3 {get; set;}
+        
+        [BsonElement("cc")]
+        public int cc {get; set;}
+        
+        [BsonElement("pci")]
+        public int pci {get; set;}
+
+        [BsonElement("earfcn")]
+        public int earfcn {get; set;}
+
+        [BsonElement("rsrp")]
+        public double rsrp {get; set;}
+
+        [BsonElement("pl")]
+        public double pl {get; set;}
+
+        [BsonElement("cfo")]
+        public double cfo {get; set;}
+
+        [BsonElement("dl_mcs")]
+        public int dl_mcs {get; set;}
+
+        [BsonElement("dl_brate")]
+        public int dl_brate {get; set;}
+
+        [BsonElement("dl_bler")]
+        public double dl_bler {get; set;}
+
+        [BsonElement("dl_snr")]
+        public float dl_snr {get; set;}
+
+        [BsonElement("ul_buff")]
+        public int ul_buff {get; set;}
+
+        [BsonElement("nof_ue")]
+        public int nof_ue {get; set;}
+
 
     }
 
