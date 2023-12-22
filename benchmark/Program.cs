ef76396251e75e0334a4d7db58b338cdd134343d
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-12-22 02:29:27 +0100

    Write aggregation test

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -16,14 +16,26 @@ namespace Generator
         static void Main(string[] args)
         {
 
+            if(!BsonClassMap.IsClassMapRegistered(typeof(UEData))){
+                Console.WriteLine($"Registering {typeof(UEData)} classmap.");
+                BsonClassMap.RegisterClassMap<UEData>();
+            }
+            if(!BsonClassMap.IsClassMapRegistered(typeof(BSData))){
+                Console.WriteLine($"Registering {typeof(BSData)} classmap.");
+                BsonClassMap.RegisterClassMap<BSData>();
+            }
+            if(!BsonClassMap.IsClassMapRegistered(typeof(Record))){
+                Console.WriteLine($"Registering {typeof(Record)} classmap.");
+                BsonClassMap.RegisterClassMap<Record>();
+            }
             start_date = DateTime.ParseExact("20230501T00:00:00Z", "yyyyMMddTHH:mm:ssZ", System.Globalization.CultureInfo.InvariantCulture).ToUniversalTime();
-            ParseArgs(args);
+           
             Connect();
-            //ResetDB();
-            //CreateTSCollection();
-            //BulkLoad(1_000);
-            //ReadTest(100);
-            AggregationTest(1, false);
+            ResetDB();
+            CreateTSCollection();
+            BulkLoad(10_000);
+            ReadTest(1_000);
+            AggregationTest(100, false);
             // ReadTest(1_000_000,0);
             //watch.Stop();
             //Console.WriteLine(watch.ElapsedMilliseconds);
@@ -52,16 +64,17 @@ namespace Generator
             }
         }
         static void Connect(){
-            foreach (var conString in contactPoints){
-                clients.Add(new MongoClient("mongodb://root:pass@localhost:27017/"));
-            }
+            // foreach (var conString in contactPoints){
+            //     clients.Add(new MongoClient("mongodb://root:pass@localhost:27017/"));
+            // }
+            clients.Add(new MongoClient("mongodb://root:pass@localhost:27017/"));
         }
         static void ResetDB(){
             var master = clients[0];
             master.DropDatabase("benchmark");
         }
         static void CreateTSCollection(){
-            clients[0].GetDatabase("benchmark").CreateCollection("metrics", new CreateCollectionOptions { TimeSeriesOptions = new TimeSeriesOptions("timestamp", "data") });
+            clients[0].GetDatabase("benchmark").CreateCollection("metrics", new CreateCollectionOptions { TimeSeriesOptions = new TimeSeriesOptions("timestamp") });
         }
         static void BulkLoad(int n){
             var data = GenerateTestData(n, start_date);
@@ -111,13 +124,14 @@ namespace Generator
                 var db = clients[0].GetDatabase("benchmark");
                 var collection = db.GetCollection<Record>("metrics");
                 var filter = Builders<Record>.Filter.Eq(x=>x.BSData.bs_id, new Random().Next(3));
-                watch.Start();
+
+                watch.Restart();
                 var doc = collection.Find<Record>(filter).First();
                 watch.Stop();
                 time_sum += watch.Elapsed.TotalSeconds;
             }
             Console.WriteLine($"Test finished after {time_sum}seconds.");
-            Console.WriteLine($"Average ops/s: {no_reads/time_sum} seconds per read.");
+            Console.WriteLine($"Average ops/s: {no_reads/time_sum}.");
         }
         static void AggregationTest(int n, bool two_weeks){
             var watch = new System.Diagnostics.Stopwatch();
@@ -126,20 +140,21 @@ namespace Generator
             var filter_builder = Builders<Record>.Filter;
             var query = filter_builder.Gte(x => x.timestamp, start_date) & filter_builder.Lte(x => x.timestamp, end_date) & filter_builder.Exists(x => x.UEData);
             double time_sum = 0;
+            
             for (int i = 0; i < n; i++){
                 var db = clients[0].GetDatabase("benchmark");
                 var collection = db.GetCollection<Record>("metrics");
-                watch.Start();
-                var res = collection.Aggregate<Record>()
-                .Match(query)
-                .Group(g => g.UEData.pci, // Group by a constant value (1) or any specific field
-                    g => new { avg = g.Average(x => x.UEData.dlul_brate) }).ToBsonDocument();//g.Average(x => x.Field1) });
+                var q = collection.Aggregate()
+                    .Match(query)
+                    .Group(g => g.UEData.pci, // Group by a constant value (1) or any specific field
+                  g => new { avg = g.Average(x => x.UEData.dlul_brate) });//g.Average(x => x.Field1) });
+                watch.Restart();
+                var res = q.ToList();
                 watch.Stop();
-                Console.WriteLine(res);
                 time_sum += watch.Elapsed.TotalSeconds;
             }
             Console.WriteLine($"Test finished after {time_sum}seconds.");
-            Console.WriteLine($"Average delay: {time_sum/n} seconds per read.");
+            Console.WriteLine($"Average ops/s: {n/time_sum}.");
         }
 
     }
