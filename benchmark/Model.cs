0cf775b44958b2c57233e1a4b6f95e59cbe67f8b
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-12-22 02:11:40 +0100

    Update data models

--- benchmark/Model.cs
+++ benchmark/Model.cs
@@ -2,51 +2,64 @@ using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization;
 
 namespace Models{
-    public class Data{
+    public class UEData{
         
+        [BsonElement("ue_id")]
+        public int ue_id {get; set;}
         [BsonElement("cc")]
-        public int cc {get; set;}
+        public int cc {get; set;} = 0;
         
         [BsonElement("pci")]
         public int pci {get; set;}
 
         [BsonElement("earfcn")]
-        public int earfcn {get; set;}
+        public int earfcn {get; set;} = 3400;
 
         [BsonElement("rsrp")]
-        public double rsrp {get; set;}
+        public int rsrp {get; set;} = new Random().Next(-80, -40);
 
         [BsonElement("pl")]
-        public double pl {get; set;}
+        public int pl {get; set;} = new Random().Next(40, 80);
 
         [BsonElement("cfo")]
-        public double cfo {get; set;}
+        public double cfo {get; set;} = new Random().NextDouble() * -1_000;
 
-        [BsonElement("dl_mcs")]
-        public int dl_mcs {get; set;}
+        [BsonElement("dlul_mcs")]
+        public double dlul_mcs {get; set;} = new Random().NextDouble() * 20;
 
-        [BsonElement("dl_brate")]
-        public int dl_brate {get; set;}
+        [BsonElement("dlul_brate")]
+        public double dlul_brate {get; set;} = new Random().NextDouble() * 50_000;
 
-        [BsonElement("dl_bler")]
-        public double dl_bler {get; set;}
+        [BsonElement("dlul_bler")]
+        public double dlul_bler {get; set;} = new Random().NextDouble() * 100;
 
         [BsonElement("dl_snr")]
-        public float dl_snr {get; set;}
+        public double dl_snr {get; set;} = new Random().NextDouble() * 5 + 10;
 
         [BsonElement("ul_buff")]
-        public int ul_buff {get; set;}
-
-        [BsonElement("nof_ue")]
-        public int nof_ue {get; set;}
-
+        public double ul_buff {get; set;} = new Random().NextDouble() * 100;
 
     }
 
+    public class BSData{
+        [BsonElement("bs_id")]
+        public int bs_id {get; set;}
+
+        [BsonElement("nof_ue")]
+        public int nof_ue {get; set;} = 5;
+        [BsonElement("dlul_brate")]
+        public double dlul_brate {get; set;} = new Random().NextDouble() * 1_500_000 + 500_000;
+    }
+
     [BsonIgnoreExtraElements]
-    public class TimeSeries{
-        [BsonElement("data")]
-        public Data data {get; set;}
+    public class Record{
+
+        [BsonIgnoreIfNull]
+        [BsonElement("ue_data")]
+        public UEData? UEData {get; set;}
+        [BsonIgnoreIfNull]
+        [BsonElement("bs_data")]
+        public BSData? BSData {get; set;}
         public DateTime timestamp {get; set;}
     }
 }
