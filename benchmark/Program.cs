76945e6c37bd8ddee76be44eab05eaed09b37b1e
Bartek Kowalski <poprostubartek.official@gmail.com>
2023-12-22 12:41:00 +0100

    Add ElasticTester class

--- benchmark/Program.cs
+++ benchmark/Program.cs
@@ -5,9 +5,101 @@ using MongoDB.Bson;
 using Models;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.IO;
+using Elastic.Clients.Elasticsearch;
+using Elastic.Transport;
+using Elastic.Clients.Elasticsearch.QueryDsl;
+using System.Security.Cryptography.X509Certificates;
+using Elastic.Clients.Elasticsearch.Aggregations;
 
 namespace Generator
 {
+
+    class ElasticTester{
+
+        private static ElasticsearchClient client;
+        private static DateTime start_date;
+        public ElasticTester(){}
+        static void SetupClient(){
+            client = new ElasticsearchClient(new Uri(""));
+            start_date = DateTime.ParseExact("20230501T00:00:00Z", "yyyyMMddTHH:mm:ssZ", System.Globalization.CultureInfo.InvariantCulture).ToUniversalTime();
+        } 
+
+        static void BulkLoad(int no_records){
+            
+            client.Indices.Create("metrics");
+            var data = GenerateTestData(no_records);
+            
+        }
+
+        static List<Record> GenerateTestData(int count){
+            var testData = new List<Record>();
+            var random = new Random();
+            Record r;
+            for (var i = 0; i < count; i++){
+              
+                for (int bs_id = 0; bs_id < 3; bs_id++){
+                        r = new Record { 
+                        timestamp = start_date.AddSeconds(i).ToUniversalTime(), 
+                        BSData = new BSData{
+                                bs_id = bs_id
+                            } 
+                        };
+                    testData.Add(r);
+                    for (int ue_id = 0; ue_id < 5; ue_id++){
+                            r = new Record { 
+                            timestamp = start_date.AddSeconds(i).ToUniversalTime(), 
+                            UEData = new UEData{
+                                    ue_id = ue_id,
+                                    pci = bs_id 
+                                } 
+                        };
+                        testData.Add(r);  
+                    }
+                }
+                
+            }
+
+            return testData;
+        }
+        static void ResetDB(){
+            client.Indices.Delete("metrics");
+        }
+        static void ReadTest(int no_reads){
+            var watch = new System.Diagnostics.Stopwatch();
+            Console.WriteLine($"Starting read test with {no_reads} reads...");
+            double time_sum = 0;
+            for (int i = 0; i < no_reads; i++){
+                
+                watch.Restart();
+                var res = client.Get<Record>(1, x => x.Index("metrics"));
+                watch.Stop();
+            }
+            Console.WriteLine($"Test finished after {watch.Elapsed.TotalSeconds}seconds.");
+            Console.WriteLine($"Average ops/s: {no_reads/watch.Elapsed.TotalSeconds}.");
+        }
+
+        static void AggregationTest(int n, bool two_weeks){
+            var watch = new System.Diagnostics.Stopwatch();
+            var end_date = two_weeks ? start_date.AddDays(14):start_date.AddDays(7);
+            Console.WriteLine($"Starting aggregation test with {n} querries ({start_date} - {end_date}...)");
+            for (int i = 0; i < n; i++){
+                var request = new SearchRequest<Record>{
+                    Query = new DateRangeQuery(Infer.Field<Record>(x => x.timestamp)){
+                        Gte = start_date,
+                        Lte = end_date
+                    },
+                    Aggregations = new AverageAggregation("avg", Infer.Field<Record>(x => x.UEData.dlul_brate))
+                };
+
+                watch.Restart();
+                var res = client.Search<Record>(request);
+                watch.Stop();
+            }
+            Console.WriteLine($"Test finished after {watch.Elapsed.TotalSeconds}seconds.");
+            Console.WriteLine($"Average ops/s: {n/watch.Elapsed.TotalSeconds}.");
+        }
+
+    }
     class Generator {
         private static List<string> contactPoints = [];
         private static List<MongoClient> clients = [];
