200baa2489a3853dcdd6ede5e97409c81870a38a
Tymon Szczepanowski <tymon.szczepanowski@gmail.com>
2024-01-03 21:18:42 +0100

    Change playbooks and configs to be aligned with cloud deployment

--- cloud-infra/main.tf
+++ cloud-infra/main.tf
@@ -1,17 +1,16 @@
 # Bucket to store website
 
-resource "google_compute_address" "db_vm1_static_ip" {
-  name = "db-vm1-static-ip"
+resource "google_compute_address" "benchmark_static_ip" {
+  name = "benchmark-static-ip"
   address_type = "EXTERNAL"
 }
 
-resource "google_compute_instance" "db_vm1" {
-  name         = "db-vm1"
-  machine_type = "e2-standard-4"
+resource "google_compute_instance" "benchmark" {
+  name         = "benchmark"
+  machine_type = "e2-medium"
   zone = var.gcp_zone
-  tags = ["db"]
   metadata = {
-    ssh-keys = "tymon_szczepanowski:${file("~/.ssh/id_ed25519.pub")}"
+    ssh-keys = "tymon_szczepanowski:${file("~/.ssh/id_ed25519.pub")}\njan_marciniak125:${file("~/.ssh/janek.pub")}\npoprostubartek_official:${file("~/.ssh/bartek.pub")}"
   }
   boot_disk {
     initialize_params {
@@ -21,20 +20,35 @@ resource "google_compute_instance" "db_vm1" {
   network_interface {
     network = "default"
     access_config {
-      nat_ip = google_compute_address.db_vm1_static_ip.address
+      nat_ip = google_compute_address.benchmark_static_ip.address
     }
   }
 }
 
-resource "google_compute_firewall" "default" {
- name    = "web-firewall"
- network = "default"
+resource "google_compute_address" "db_vm_static_ip" {
+  count = 3
+  name = "db-vm-${format("%d", count.index + 1)}-static-ip"
+  address_type = "EXTERNAL"
+}
 
- allow {
-   protocol = "tcp"
-   ports    = ["27017"]
- }
-
- source_ranges = ["0.0.0.0/0"]
- target_tags = ["db"]
+resource "google_compute_instance" "db_vm" {
+  count = 3
+  name = "db-vm-${format("%d", count.index + 1)}"
+  machine_type = "e2-standard-2"
+  zone = var.gcp_zone
+  tags = ["db"]
+  metadata = {
+    ssh-keys = "tymon_szczepanowski:${file("~/.ssh/id_ed25519.pub")}\njan_marciniak125:${file("~/.ssh/janek.pub")}\npoprostubartek_official:${file("~/.ssh/bartek.pub")}"
+  }
+  boot_disk {
+    initialize_params {
+      image = "almalinux-cloud/almalinux-8"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.db_vm_static_ip["${count.index}"].address
+    }
+  }
 }
